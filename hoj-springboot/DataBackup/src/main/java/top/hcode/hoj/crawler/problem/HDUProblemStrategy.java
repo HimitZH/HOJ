16d46f1f6ec7ec3c18185e8d2c3ce3487a7ac334
Himit_ZH <372347736@qq.com>
2021-08-05 22:36:08 +0800

    修改POJ和HDU题目图片路径

--- hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/crawler/problem/HDUProblemStrategy.java
+++ hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/crawler/problem/HDUProblemStrategy.java
@@ -38,7 +38,7 @@ public class HDUProblemStrategy extends ProblemStrategy {
         info.setTimeLimit(Integer.parseInt(ReUtil.get("(\\d*) MS", html, 1)));
         info.setMemoryLimit(Integer.parseInt(ReUtil.get("/(\\d*) K", html, 1)) / 1024);
         info.setDescription(ReUtil.get(">Problem Description</div> <div class=.*?>([\\s\\S]*?)</div>", html, 1)
-                .replaceAll("src=\"", "src=\"" + HOST));
+                .replaceAll("src=\"[../]*", "src=\"" + HOST + "/"));
         info.setInput(ReUtil.get(">Input</div> <div class=.*?>([\\s\\S]*?)</div>", html, 1));
         info.setOutput(ReUtil.get(">Output</div> <div class=.*?>([\\s\\S]*?)</div>", html, 1));
         StringBuilder sb = new StringBuilder("<input>");
