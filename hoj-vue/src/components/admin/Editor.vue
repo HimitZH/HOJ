3e8b5d36d3b87a3e8d4d29092b43303de7008c4b
Himit_ZH <372347736@qq.com>
2021-12-27 20:46:32 +0800

    delete user upload md img

--- hoj-vue/src/components/admin/Editor.vue
+++ hoj-vue/src/components/admin/Editor.vue
@@ -6,6 +6,7 @@
       @imgDel="$imgDel"
       :ishljs="true"
       :autofocus="false"
+      :toolbars="toolbars"
       v-model="currentValue"
       codeStyle="arduino-light"
     >
@@ -46,11 +47,54 @@ export default {
     return {
       currentValue: this.value,
       img_file: {},
+      toolbars: {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: false, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: true, // 全屏编辑
+        readmodel: true, // 沉浸式阅读
+        htmlcode: true, // 展示html源码
+        help: true, // 帮助
+        /* 1.3.5 */
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        save: true, // 保存（触发events中的save事件）
+        /* 1.4.2 */
+        navigation: true, // 导航目录
+        /* 2.1.8 */
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        /* 2.2.1 */
+        subfield: true, // 单双栏模式
+        preview: true, // 预览
+      },
     };
+  },
+  created() {
+    if (this.isAdminRole) {
+      this.toolbars.imagelink = true;
+    }
   },
   methods: {
     // 将图片上传到服务器，返回地址替换到md中
     $imgAdd(pos, $file) {
+      if (!this.isAdminRole) {
+        return;
+      }
       var formdata = new FormData();
       formdata.append('image', $file);
       //将下面上传接口替换为你自己的服务器接口
@@ -103,6 +147,9 @@ export default {
       });
     },
   },
+  computed: {
+    ...mapGetters(['isAdminRole']),
+  },
   watch: {
     value(val) {
       if (this.currentValue !== val) {
@@ -117,9 +164,13 @@ export default {
         });
       }
     },
-  },
-  computed: {
-    ...mapGetters(['isAdminRole']),
+    isAdminRole(val) {
+      if (!val) {
+        this.toolbars.imagelink = false;
+      } else {
+        this.toolbars.imagelink = true;
+      }
+    },
   },
 };
 </script>
