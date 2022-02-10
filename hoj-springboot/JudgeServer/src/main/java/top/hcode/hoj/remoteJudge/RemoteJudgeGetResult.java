ac3ec82d26004d8966dc354296c493e945e21e3b
Himit_ZH <372347736@qq.com>
2022-02-10 19:52:48 +0800

    add cf judge result check

--- hoj-springboot/JudgeServer/src/main/java/top/hcode/hoj/remoteJudge/RemoteJudgeGetResult.java
+++ hoj-springboot/JudgeServer/src/main/java/top/hcode/hoj/remoteJudge/RemoteJudgeGetResult.java
@@ -49,10 +49,11 @@ public class RemoteJudgeGetResult {
             @Override
             public void run() {
 
-                if (count.get() > 60) { // 超过60次失败则判为提交失败
+                if (count.get() >= 60) { // 超过60次失败则判为提交失败
                     // 更新此次提交状态为提交失败！
                     UpdateWrapper<Judge> judgeUpdateWrapper = new UpdateWrapper<>();
                     judgeUpdateWrapper.set("status", Constants.Judge.STATUS_SUBMITTED_FAILED.getStatus())
+                            .set("error_message", "Waiting for remote judge result exceeds the maximum number of times, please try submitting again!")
                             .eq("submit_id", remoteJudgeDTO.getJudgeId());
                     judgeService.update(judgeUpdateWrapper);
 
@@ -74,93 +75,97 @@ public class RemoteJudgeGetResult {
                 }
 
                 count.getAndIncrement();
+
+                RemoteJudgeRes remoteJudgeRes;
                 try {
-                    RemoteJudgeRes remoteJudgeRes = remoteJudgeStrategy.result();
-                    Integer status = remoteJudgeRes.getStatus();
-                    if (status.intValue() != Constants.Judge.STATUS_PENDING.getStatus() &&
-                            status.intValue() != Constants.Judge.STATUS_JUDGING.getStatus() &&
-                            status.intValue() != Constants.Judge.STATUS_COMPILING.getStatus()) {
-                        log.info("[{}] Get Result Successfully! Status:[{}]", remoteJudgeDTO.getOj(), status);
+                    remoteJudgeRes = remoteJudgeStrategy.result();
+                } catch (Exception e) {
+                    if (count.get() == 60) {
+                        log.error("The Error of getting the `remote judge` result:", e);
+                    }
+                    return;
+                }
 
-                        changeRemoteJudgeLock(remoteJudgeDTO.getOj(),
-                                remoteJudgeDTO.getUsername(),
-                                remoteJudgeDTO.getServerIp(),
-                                remoteJudgeDTO.getServerPort(),
-                                remoteJudgeDTO.getSubmitId());
+                Integer status = remoteJudgeRes.getStatus();
+                if (status.intValue() != Constants.Judge.STATUS_PENDING.getStatus() &&
+                        status.intValue() != Constants.Judge.STATUS_JUDGING.getStatus() &&
+                        status.intValue() != Constants.Judge.STATUS_COMPILING.getStatus()) {
+                    log.info("[{}] Get Result Successfully! Status:[{}]", remoteJudgeDTO.getOj(), status);
 
-                        Integer time = remoteJudgeRes.getTime();
-                        Integer memory = remoteJudgeRes.getMemory();
-                        String errorInfo = remoteJudgeRes.getErrorInfo();
-                        Judge judge = new Judge();
+                    changeRemoteJudgeLock(remoteJudgeDTO.getOj(),
+                            remoteJudgeDTO.getUsername(),
+                            remoteJudgeDTO.getServerIp(),
+                            remoteJudgeDTO.getServerPort(),
+                            remoteJudgeDTO.getSubmitId());
 
-                        judge.setSubmitId(remoteJudgeDTO.getJudgeId())
-                                .setStatus(status)
-                                .setTime(time)
-                                .setMemory(memory)
-                                .setJudger(name);
+                    Integer time = remoteJudgeRes.getTime();
+                    Integer memory = remoteJudgeRes.getMemory();
+                    String errorInfo = remoteJudgeRes.getErrorInfo();
+                    Judge judge = new Judge();
 
-                        if (status.intValue() == Constants.Judge.STATUS_COMPILE_ERROR.getStatus()) {
-                            judge.setErrorMessage(errorInfo);
-                        } else if (status.intValue() == Constants.Judge.STATUS_SYSTEM_ERROR.getStatus()) {
-                            judge.setErrorMessage("There is something wrong with the " + remoteJudgeDTO.getOj() + ", please try again later");
-                        }
+                    judge.setSubmitId(remoteJudgeDTO.getJudgeId())
+                            .setStatus(status)
+                            .setTime(time)
+                            .setMemory(memory)
+                            .setJudger(name);
 
-                        // 如果是比赛题目，需要特别适配OI比赛的得分 除AC给100 其它结果给0分
-                        if (remoteJudgeDTO.getCid() != 0) {
-                            int score = 0;
-
-                            if (judge.getStatus().intValue() == Constants.Judge.STATUS_ACCEPTED.getStatus()) {
-                                score = 100;
-                            }
-
-                            judge.setScore(score);
-                            // 写回数据库
-                            judgeService.updateById(judge);
-                            // 同步其它表
-                            judgeService.updateOtherTable(remoteJudgeDTO.getJudgeId(),
-                                    status,
-                                    remoteJudgeDTO.getCid(),
-                                    remoteJudgeDTO.getUid(),
-                                    remoteJudgeDTO.getPid(),
-                                    score,
-                                    judge.getTime());
-
-                        } else {
-                            judgeService.updateById(judge);
-                            // 同步其它表
-                            judgeService.updateOtherTable(remoteJudgeDTO.getJudgeId(),
-                                    status,
-                                    remoteJudgeDTO.getCid(),
-                                    remoteJudgeDTO.getUid(),
-                                    remoteJudgeDTO.getPid(),
-                                    null,
-                                    null);
-                        }
-
-                        Future future = futureTaskMap.get(key);
-                        if (future != null) {
-                            future.cancel(true);
-                            futureTaskMap.remove(key);
-                        }
-                    } else {
-
-                        Judge judge = new Judge();
-                        judge.setSubmitId(remoteJudgeDTO.getJudgeId())
-                                .setStatus(status)
-                                .setJudger(name);
-                        // 写回数据库
-                        judgeService.updateById(judge);
+                    if (status.intValue() == Constants.Judge.STATUS_COMPILE_ERROR.getStatus()) {
+                        judge.setErrorMessage(errorInfo);
+                    } else if (status.intValue() == Constants.Judge.STATUS_SYSTEM_ERROR.getStatus()) {
+                        judge.setErrorMessage("There is something wrong with the " + remoteJudgeDTO.getOj() + ", please try again later");
                     }
 
-                } catch (Exception e) {
-                    log.error("The Error of getting the `remote judge` result:", e);
-                    throw e;
+                    // 如果是比赛题目，需要特别适配OI比赛的得分 除AC给100 其它结果给0分
+                    if (remoteJudgeDTO.getCid() != 0) {
+                        int score = 0;
+
+                        if (judge.getStatus().intValue() == Constants.Judge.STATUS_ACCEPTED.getStatus()) {
+                            score = 100;
+                        }
+
+                        judge.setScore(score);
+                        // 写回数据库
+                        judgeService.updateById(judge);
+                        // 同步其它表
+                        judgeService.updateOtherTable(remoteJudgeDTO.getJudgeId(),
+                                status,
+                                remoteJudgeDTO.getCid(),
+                                remoteJudgeDTO.getUid(),
+                                remoteJudgeDTO.getPid(),
+                                score,
+                                judge.getTime());
+
+                    } else {
+                        judgeService.updateById(judge);
+                        // 同步其它表
+                        judgeService.updateOtherTable(remoteJudgeDTO.getJudgeId(),
+                                status,
+                                remoteJudgeDTO.getCid(),
+                                remoteJudgeDTO.getUid(),
+                                remoteJudgeDTO.getPid(),
+                                null,
+                                null);
+                    }
+
+                    Future future = futureTaskMap.get(key);
+                    if (future != null) {
+                        future.cancel(true);
+                        futureTaskMap.remove(key);
+                    }
+                } else {
+
+                    Judge judge = new Judge();
+                    judge.setSubmitId(remoteJudgeDTO.getJudgeId())
+                            .setStatus(status)
+                            .setJudger(name);
+                    // 写回数据库
+                    judgeService.updateById(judge);
                 }
 
             }
         };
         ScheduledFuture<?> beeperHandle = scheduler.scheduleWithFixedDelay(
-                getResultTask, 0, 3, TimeUnit.SECONDS);
+                getResultTask, 0, 2500, TimeUnit.MILLISECONDS);
         futureTaskMap.put(key, beeperHandle);
     }
 
