654dba6655cc6ca21294ae129f97878db5b42ef1
Himit_ZH <372347736@qq.com>
2022-02-20 15:57:08 +0800

    change problem list query

--- hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/pojo/vo/ProblemVo.java
+++ hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/pojo/vo/ProblemVo.java
@@ -13,7 +13,7 @@ import java.util.List;
  * @Date: 2020/10/27 14:14
  * @Description:
  */
-@ApiModel(value="题目列表查询对象ProblemVo", description="")
+@ApiModel(value = "题目列表查询对象ProblemVo", description = "")
 @Data
 public class ProblemVo implements Serializable {
 
@@ -38,36 +38,49 @@ public class ProblemVo implements Serializable {
     // 以下为题目做题情况
 
     @ApiModelProperty(value = "该题总提交数")
-    private Integer total;
+    private Integer total = 0;
 
     @ApiModelProperty(value = "通过提交数")
-    private Integer ac;
+    private Integer ac = 0;
 
     @ApiModelProperty(value = "空间超限提交数")
-    private Integer mle;
+    private Integer mle = 0;
 
     @ApiModelProperty(value = "时间超限提交数")
-    private Integer tle;
+    private Integer tle = 0;
 
     @ApiModelProperty(value = "运行错误提交数")
-    private Integer re;
+    private Integer re = 0;
 
     @ApiModelProperty(value = "格式错误提交数")
-    private Integer pe;
+    private Integer pe = 0;
 
     @ApiModelProperty(value = "编译错误提交数")
-    private Integer ce;
+    private Integer ce = 0;
 
     @ApiModelProperty(value = "答案错误提交数")
-    private Integer wa;
+    private Integer wa = 0;
 
     @ApiModelProperty(value = "系统错误提交数")
-    private Integer se;
+    private Integer se = 0;
 
     @ApiModelProperty(value = "该IO题目分数总和")
-    private Integer pa;
+    private Integer pa = 0;
 
     @ApiModelProperty(value = "IO题目总分数")
     private Integer score;
+
+    public void setProblemCountVo(ProblemCountVo problemCountVo) {
+        this.total = problemCountVo.getTotal() == null ? 0 : problemCountVo.getTotal();
+        this.ac = problemCountVo.getAc() == null ? 0 : problemCountVo.getAc();
+        this.mle = problemCountVo.getMle() == null ? 0 : problemCountVo.getMle();
+        this.tle = problemCountVo.getTle() == null ? 0 : problemCountVo.getTle();
+        this.re = problemCountVo.getRe() == null ? 0 : problemCountVo.getRe();
+        this.pe = problemCountVo.getPe() == null ? 0 : problemCountVo.getPe();
+        this.ce = problemCountVo.getCe() == null ? 0 : problemCountVo.getCe();
+        this.wa = problemCountVo.getWa() == null ? 0 : problemCountVo.getWa();
+        this.se = problemCountVo.getSe() == null ? 0 : problemCountVo.getSe();
+        this.pa = problemCountVo.getPa() == null ? 0 : problemCountVo.getPa();
+    }
 
 }
