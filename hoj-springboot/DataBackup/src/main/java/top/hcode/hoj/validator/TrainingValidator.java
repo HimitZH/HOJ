16b48ad99103f419f756ffadc76f0e16cbb01e69
Himit_ZH <372347736@qq.com>
2022-03-21 21:05:23 +0800

    fix private training bug

---
+++ hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/validator/TrainingValidator.java
@@ -0,0 +1,83 @@
+package top.hcode.hoj.validator;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.springframework.stereotype.Component;
+import top.hcode.hoj.common.exception.StatusAccessDeniedException;
+import top.hcode.hoj.common.exception.StatusForbiddenException;
+import top.hcode.hoj.dao.training.TrainingRegisterEntityService;
+import top.hcode.hoj.pojo.entity.training.Training;
+import top.hcode.hoj.pojo.entity.training.TrainingRegister;
+import top.hcode.hoj.pojo.vo.UserRolesVo;
+import top.hcode.hoj.utils.Constants;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Himit_ZH
+ * @Date: 2022/3/21 20:55
+ * @Description:
+ */
+@Component
+public class TrainingValidator {
+
+    @Resource
+    private TrainingRegisterEntityService trainingRegisterEntityService;
+
+    public void checkTrainingAuth(Training training) throws StatusAccessDeniedException, StatusForbiddenException {
+        Session session = SecurityUtils.getSubject().getSession();
+        UserRolesVo userRolesVo = (UserRolesVo) session.getAttribute("userInfo");
+        checkTrainingAuth(training, userRolesVo);
+    }
+
+
+    public void checkTrainingAuth(Training training, UserRolesVo userRolesVo) throws StatusAccessDeniedException, StatusForbiddenException {
+        if (Constants.Training.AUTH_PRIVATE.getValue().equals(training.getAuth())) {
+            if (userRolesVo == null) {
+                throw new StatusAccessDeniedException("该训练属于私有题单，请先登录以校验权限！");
+            }
+            boolean isRoot = SecurityUtils.getSubject().hasRole("root"); // 是否为超级管理员
+            boolean isAuthor = training.getAuthor().equals(userRolesVo.getUsername()); // 是否为该私有训练的创建者
+
+            if (!isRoot && !isAuthor) { // 如果两者都不是，需要做注册权限校验
+                checkTrainingRegister(training.getId(), userRolesVo.getUid());
+            }
+        }
+    }
+
+    private void checkTrainingRegister(Long tid, String uid) throws StatusAccessDeniedException, StatusForbiddenException {
+        QueryWrapper<TrainingRegister> trainingRegisterQueryWrapper = new QueryWrapper<>();
+        trainingRegisterQueryWrapper.eq("tid", tid);
+        trainingRegisterQueryWrapper.eq("uid", uid);
+        TrainingRegister trainingRegister = trainingRegisterEntityService.getOne(trainingRegisterQueryWrapper, false);
+
+        if (trainingRegister == null) {
+            throw new StatusAccessDeniedException("该训练属于私有，请先使用专属密码注册！");
+        }
+
+        if (!trainingRegister.getStatus()) {
+            throw new StatusForbiddenException("错误：你已被禁止参加该训练！");
+        }
+    }
+
+    public boolean isInTrainingOrAdmin(Training training, UserRolesVo userRolesVo) throws StatusAccessDeniedException {
+        if (Constants.Training.AUTH_PRIVATE.getValue().equals(training.getAuth())) {
+            if (userRolesVo == null) {
+                throw new StatusAccessDeniedException("该训练属于私有题单，请先登录以校验权限！");
+            }
+            boolean isRoot = SecurityUtils.getSubject().hasRole("root"); // 是否为超级管理员
+            boolean isAuthor = training.getAuthor().equals(userRolesVo.getUsername()); // 是否为该私有训练的创建者
+
+            if (!isRoot && !isAuthor) { // 如果两者都不是，需要做注册权限校验
+                QueryWrapper<TrainingRegister> trainingRegisterQueryWrapper = new QueryWrapper<>();
+                trainingRegisterQueryWrapper.eq("tid", training.getId());
+                trainingRegisterQueryWrapper.eq("uid", userRolesVo.getUid());
+                TrainingRegister trainingRegister = trainingRegisterEntityService.getOne(trainingRegisterQueryWrapper, false);
+
+                return trainingRegister != null && trainingRegister.getStatus();
+            }
+        }
+        return true;
+    }
+}
