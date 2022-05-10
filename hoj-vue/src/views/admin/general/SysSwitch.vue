f3fee7401dd8d1fd0e3b641dce289ba2cccd8f1e
Himit_ZH <372347736@qq.com>
2022-05-10 16:50:15 +0800

    update frontend

--- hoj-vue/src/views/admin/general/SysSwitch.vue
+++ hoj-vue/src/views/admin/general/SysSwitch.vue
@@ -5,18 +5,18 @@
         <span class="panel-title home-title">{{ $t('m.System_Switch') }}</span>
       </div>
       <el-row :gutter="20">
-        <el-col :xs="24" :md="8">
+        <el-col :xs="24" :md="12">
             <el-card>
                 <div slot="header">
                     <span class="switch-item-title">{{ $t('m.Judge_Config') }}</span>
                 </div>
                 <el-form
                     :label-position="labelPosition"
-                    label-width="250px"
+                    label-width="320px"
                     ref="form"
                     :model="switchConfig"
                 >
-                    <el-form-item :label="$t('m.Open_Public_Judge')" label-width="250px">
+                    <el-form-item :label="$t('m.Open_Public_Judge')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openPublicJudge"
                             active-color="#13ce66"
@@ -24,7 +24,7 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Open_Group_Judge')" label-width="250px">
+                    <el-form-item :label="$t('m.Open_Group_Judge')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openGroupJudge"
                             active-color="#13ce66"
@@ -32,7 +32,7 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Open_Contest_Judge')" label-width="250px">
+                    <el-form-item :label="$t('m.Open_Contest_Judge')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openContestJudge"
                             active-color="#13ce66"
@@ -40,7 +40,7 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Non_Contest_Submission_Frequency')" label-width="250px">
+                    <el-form-item :label="$t('m.Non_Contest_Submission_Frequency')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultSubmitInterval" 
                          :min="0">
@@ -57,18 +57,18 @@
                 >
             </el-card>
         </el-col>
-        <el-col :xs="24" :md="8">
+        <el-col :xs="24" :md="12">
             <el-card class="card-top">
                 <div slot="header">
                     <span class="switch-item-title">{{ $t('m.Discussion_Config') }}</span>
                 </div>
                 <el-form
                     :label-position="labelPosition"
-                    label-width="250px"
+                    label-width="320px"
                     ref="form"
                     :model="switchConfig"
                 >
-                     <el-form-item :label="$t('m.Open_Public_Discussion')" label-width="250px">
+                     <el-form-item :label="$t('m.Open_Public_Discussion')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openPublicDiscussion"
                             active-color="#13ce66"
@@ -76,7 +76,7 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Open_Group_Discussion')" label-width="250px">
+                    <el-form-item :label="$t('m.Open_Group_Discussion')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openGroupDiscussion"
                             active-color="#13ce66"
@@ -84,7 +84,7 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Open_Contest_Comment')" label-width="250px">
+                    <el-form-item :label="$t('m.Open_Contest_Comment')" label-width="320px">
                         <el-switch
                             v-model="switchConfig.openContestComment"
                             active-color="#13ce66"
@@ -92,19 +92,19 @@
                         >
                         </el-switch>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Number_of_AC_required_for_ordinary_users_to_post')" label-width="250px">
+                    <el-form-item :label="$t('m.Number_of_AC_required_for_ordinary_users_to_post')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateDiscussionACInitValue" 
                          :min="0">
                          </el-input-number>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Number_of_posts_that_users_can_create_per_day')" label-width="250px">
+                    <el-form-item :label="$t('m.Number_of_posts_that_users_can_create_per_day')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateDiscussionDailyLimit" 
                          :min="0">
                          </el-input-number>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Number_of_AC_required_for_Comment_of_ordinary_users')" label-width="250px">
+                    <el-form-item :label="$t('m.Number_of_AC_required_for_Comment_of_ordinary_users')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateCommentACInitValue" 
                          :min="0">
@@ -121,30 +121,30 @@
                 >
             </el-card>
         </el-col>
-        <el-col :xs="24" :md="8">
+        <el-col :xs="24" :md="12">
             <el-card class="card-top">
                 <div slot="header">
                     <span class="switch-item-title">{{ $t('m.Group_Config') }}</span>
                 </div>
                 <el-form
                     :label-position="labelPosition"
-                    label-width="250px"
+                    label-width="320px"
                     ref="form"
                     :model="switchConfig"
                 >
-                    <el-form-item :label="$t('m.Number_of_Groups_that_users_can_create_per_day')" label-width="250px">
+                    <el-form-item :label="$t('m.Number_of_Groups_that_users_can_create_per_day')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateGroupDailyLimit" 
                          :min="0">
                          </el-input-number>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Total_number_of_groups_that_ordinary_users_can_create')" label-width="250px">
+                    <el-form-item :label="$t('m.Total_number_of_groups_that_ordinary_users_can_create')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateGroupLimit" 
                          :min="0">
                          </el-input-number>
                     </el-form-item>
-                    <el-form-item :label="$t('m.Number_of_AC_required_for_ordinary_users_to_create_group')" label-width="250px">
+                    <el-form-item :label="$t('m.Number_of_AC_required_for_ordinary_users_to_create_group')" label-width="320px">
                         <el-input-number
                          v-model="switchConfig.defaultCreateGroupACInitValue" 
                          :min="0">
