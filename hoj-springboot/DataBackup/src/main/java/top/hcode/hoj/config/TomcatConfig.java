381392d37b9aea52713f8e03528bc96d4438d997
Himit_ZH <372347736@qq.com>
2022-06-11 22:06:25 +0800

    fix get spe params

---
+++ hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/config/TomcatConfig.java
@@ -0,0 +1,30 @@
+package top.hcode.hoj.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Himit_ZH
+ * @Date 2022/6/11
+ * 允许请求路径出现[] \ 等特殊字符
+ */
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+                connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+                connector.setProperty("rejectIllegalHeader", "false");
+            }
+        });
+        return factory;
+    }
+}
