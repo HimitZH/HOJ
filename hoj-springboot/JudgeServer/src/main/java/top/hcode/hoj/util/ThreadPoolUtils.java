4263eacacd6ea37fd6ce6128c15b8e8eed5eefef
Himit_ZH <372347736@qq.com>
2022-10-09 20:47:57 +0800

    update: threadpool size

--- hoj-springboot/JudgeServer/src/main/java/top/hcode/hoj/util/ThreadPoolUtils.java
+++ hoj-springboot/JudgeServer/src/main/java/top/hcode/hoj/util/ThreadPoolUtils.java
@@ -17,7 +17,7 @@ public class ThreadPoolUtils {
         //手动创建线程池.
         executorService = new ThreadPoolExecutor(
                 cpuNum, // 核心线程数
-                cpuNum, // 最大线程数。最多几个线程并发。
+                cpuNum + 1, // 最大线程数。最多几个线程并发。
                 3,//当非核心线程无任务时，几秒后结束该线程
                 TimeUnit.SECONDS,// 结束线程时间单位
                 new LinkedBlockingDeque<>(200 * cpuNum), //阻塞队列，限制等候线程数
